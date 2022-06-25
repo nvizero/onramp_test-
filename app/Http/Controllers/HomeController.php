8b223e00be169d7c5d88f0acd2d26d05b9347900
withvictor <withvictor@gmail.com>
2022-06-25 15:19:35 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+ 
+class HomeController extends Controller
+{
+    public function index(){
+
+        return view('home.index');
+    }
+}
